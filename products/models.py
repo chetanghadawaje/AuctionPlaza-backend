825463612ee13ea7c3ffc8acd429123fad8d42c5
chetan <chetanghadawaje@gmail.com>
2024-04-08 14:55:11 +0530

    Change models.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
+import uuid
 from django.db import models
+from auction_plaza.utils.models_utils import BaseModel
+from users.models import User
 
-# Create your models here.
+
+
+class ProductCategory(BaseModel):
+    name = models.CharField(max_length=100, unique=True)  
+    description = models.TextField(blank=False)  
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    initial_price = models.DecimalField(max_digits=10, decimal_places=2)
+    auction_date_time = models.DateTimeField()
+    STATUS_CHOICES = (
+        ('upcoming', 'Upcoming'),
+        ('inprocess', 'Inprocess'),
+        ('completed', 'Completed'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='upcoming')
+    bid_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField()
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.contrib.auth.models import UserManager
+
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password, mobile_number):
+        user = self.create_user(email, password)
+        user.profile_picture = mobile_number
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    username = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=10)
